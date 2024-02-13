572c06a1d93f84d96ba0952b04f46ca1d29fbd7d
Rhodz <Rhodz@DESKTOP-INQKN1K>
2024-02-14 00:50:27 +0800

    CHAEYOUNG

--- src/Chaeyoung/Chaeyoung.java
+++ src/Chaeyoung/Chaeyoung.java
@@ -2,19 +2,25 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package chaeyeong;
-
+package Chaeyoung;
+import java.util.Scanner;
 /**
  *
  * @author Rhodz
  */
-public class Chaeyeong {
+public class Chaeyoung {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
         
+        String fName;
+        
+        System.out.print("Enter First Name: ");
+        fName = scn.nextLine();
+        System.out.println("First Name: "+fName);
     }
     
 }
