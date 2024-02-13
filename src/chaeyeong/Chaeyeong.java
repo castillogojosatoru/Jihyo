bcccce4e1f177070b165bf125ab83102df5d6c52
Rhodz <Rhodz@DESKTOP-INQKN1K>
2024-02-14 00:46:10 +0800

    CHAEYOUNG

---
+++ src/chaeyeong/Chaeyeong.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package chaeyeong;
+
+/**
+ *
+ * @author Rhodz
+ */
+public class Chaeyeong {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
