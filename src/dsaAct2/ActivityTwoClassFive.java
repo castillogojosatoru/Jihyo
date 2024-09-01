07d08b80aaa457150b16bd78d8ebc1726cecd746
Rhodz <Rhodz@DESKTOP-INQKN1K>
2024-09-01 19:14:06 +0800

    AGBON

---
+++ src/dsaAct2/ActivityTwoClassFive.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dsaAct2;
+
+/**
+ *
+ * @author Rhodz
+ */
+public class ActivityTwoClassFive extends ActivityTwoClassFour {
+    
+    public ActivityTwoClassFive(int Size) {
+        super(Size);
+    }
+    
+    public void addStack() {
+        for (int i = 0; i <= top; i++) {
+            if (!isFull()) {
+                System.out.println("STACK COLLECTION ADDED TO ANOTHER STACK COLLECTION: " + Stack[i]);
+                stacknew.push(Stack[i]);
+            }
+            else {
+                System.out.println("STACK IS FULL.");
+            }
+        }
+    }
+    
+    public void newloopstack() {
+        if (isEmpty()) {
+            System.out.println("STACK IS EMPTY.");
+        }
+        else {
+            System.out.println("NEW STACK ELEMENT: ");
+            int loopLimit = Math.min(top, stacknew.size() - 1);
+            for (int i = 0; i <= loopLimit; i++) {
+                System.out.println(stacknew.get(i));
+            }
+        }
+    }
+}
---
+++ src/dsaAct2/ActivityTwoClassOne.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dsaAct2;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author Rhodz
+ */
+public class ActivityTwoClassOne {
+    int max;
+    double[] Stack;
+    int top;
+    Stack<Double> stacknew = new Stack<>();
+    
+    public ActivityTwoClassOne(int Size) {
+        max = Size;
+        Stack = new double[max];
+        top = -1;
+    }
+    public void push(double number) {
+        if (!isFull()) {
+            Stack[++top] = number;
+        }
+        else {
+            System.out.println("STACK IS FULL.");
+        }
+    }
+    public double pop() {
+        if (!isEmpty()) {
+            return Stack[top--];
+        }
+        else {
+            System.out.println("STACK IS EMPTY.");
+            return -1;
+        }
+    }
+    public void stackloop() {
+        if (isEmpty()) {
+            System.out.println("STACK IS EMPTY.");
+        }
+        else {
+            System.out.println("STACK: ");
+            for (int i = top; i >= 0; i--) {
+                System.out.println(Stack[i]);
+            }
+        }
+    }
+    public boolean isEmpty() {
+        return (top == -1);
+    }
+    public boolean isFull() {
+        return (top == max - 1);
+    }
+}
