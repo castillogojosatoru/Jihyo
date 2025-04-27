69745637feabdc68c4dcaa0af7b0518c38fbe6b8
Admin <Admin@MSI>
2025-04-27 16:11:16 +0800

    Button Panel switch

---
+++ src/missnakita/mainClass.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package missnakita;
+
+import ui.task;
+
+/**
+ *
+ * @author Admin
+ */
+public class mainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        task t = new task();
+        
+        t.setVisible(true);
+    }
+    
+}
