b00ecd9b9398e6c5f509c59503ca31bcda012fa3
Admin <Admin@MSI>
2025-04-27 16:14:30 +0800

    Laboratory Exercise 2.2 Lab1 and Lab2

---
+++ src/mahalKita/mainClass.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mahalKita;
+
+import spa.labtask1;
+import spa2.labtask2;
+
+/**
+ *
+ * @author Admin
+ */
+public class mainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        labtask1 flowlayout = new labtask1();
+        labtask2 gridlayout = new labtask2();
+        flowlayout.setVisible(true);
+        gridlayout.setVisible(true);
+    }
+    
+}
