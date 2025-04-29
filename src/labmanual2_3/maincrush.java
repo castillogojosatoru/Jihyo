79123624a054adaf6a41c754f37e5fa6d3f239b1
Admin <Admin@MSI>
2025-04-29 23:56:55 +0800

    Laboratory Exercise 2.3 Lab Task 4.1

---
+++ src/labmanual2_3/maincrush.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labmanual2_3;
+
+/**
+ *
+ * @author Admin
+ */
+public class maincrush {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        moodbuttons mb = new moodbuttons();
+        
+        mb.setVisible(true);
+    }
+    
+}
