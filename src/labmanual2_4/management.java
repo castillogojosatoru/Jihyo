f3702a2c9085870fc4a6dbb187af86edcfc2f056
Admin <Admin@MSI>
2025-05-06 21:03:20 +0800

    Employee Management

---
+++ src/labmanual2_4/management.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package labmanual2_4;
+
+/**
+ *
+ * @author Admin
+ */
+public class management {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
